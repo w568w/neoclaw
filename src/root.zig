e922a1e2941be2c218ff4acfd37f17546e05de37
w568w <1278297578@qq.com>
2026-03-11 23:41:57 +0800

    remove: demo code

--- src/root.zig
+++ src/root.zig
@@ -12,18 +12,3 @@ pub const tools = struct {
     pub const file_write = @import("tools/file_write.zig");
     pub const ask_user = @import("tools/ask_user.zig");
 };
-
-/// This is a documentation comment to explain the `printAnotherMessage` function below.
-///
-/// Accepting an `Io.Writer` instance is a handy way to write reusable code.
-pub fn printAnotherMessage(writer: *Io.Writer) Io.Writer.Error!void {
-    try writer.print("Run `zig build test` to run the tests.\n", .{});
-}
-
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
-
-test "basic add functionality" {
-    try std.testing.expect(add(3, 7) == 10);
-}
